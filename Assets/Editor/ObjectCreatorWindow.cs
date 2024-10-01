5aa9cc8d43b36d8325e09b75293215290b74fd13
Sam <samabel18@gmail.com>
2024-10-01 17:13:59 -0400

    Auto stash before merge of "main" & "origin/main" (Place Object Feature)

--- Assets/Editor/ObjectCreatorWindow.cs
+++ Assets/Editor/ObjectCreatorWindow.cs
@@ -1,25 +1,53 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(ObjectPlacer))]
+public enum TypeOfObject { Primitive, Prefab }
+
+//[CustomEditor(typeof(ObjectPlacer))]
 public class ObjectCreatorWindow : EditorWindow
 {
     private string objectName = "New Object";
     private float objectScale = 1f;
     private Vector3 objectPosition = Vector3.zero;
 
-    private enum ObjectType { Primitive, Prefab }
-    private ObjectType selectedObjectType = ObjectType.Primitive;
+    private TypeOfObject selectedObjectType = TypeOfObject.Primitive;
 
     private PrimitiveType selectedPrimitiveType = PrimitiveType.Cube;
 
     private GameObject selectedPrefab;
+    private GameObject previewObject;
+
+    private Material[] originalMaterials = null;
+    private Renderer[] originalRenderers = null;
+
     private bool showObjectSettings = false;
+    private bool isPlacingObject = false;
+
+    private float objectHeight = 0f;
+
+    Event currentEvent;
+    private LayerMask previewLayerMask;
+    private const string PREVIEW_LAYER = "Preview";
 
     [MenuItem("Tools/Object Creator")]
     public static void ShowWindow()
     {
         GetWindow<ObjectCreatorWindow>("Object Creator");
+    }
+
+    private void OnEnable()
+    {
+        if (!LayerExists(PREVIEW_LAYER))
+        {
+            CreatePreviewLayer();
+        }
+        previewLayerMask = ~LayerMask.GetMask(PREVIEW_LAYER);
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnSceneGUI;
     }
 
     private void OnGUI()
@@ -50,7 +78,7 @@ public class ObjectCreatorWindow : EditorWindow
     {
         GUILayout.Label("Select Object Type", EditorStyles.boldLabel);
 
-        selectedObjectType = (ObjectType)EditorGUILayout.EnumPopup("Object Type", selectedObjectType);
+        selectedObjectType = (TypeOfObject)EditorGUILayout.EnumPopup("Object Type", selectedObjectType);
     }
 
     private void InstantiateSelectedGameObject()
@@ -59,7 +87,7 @@ public class ObjectCreatorWindow : EditorWindow
 
         switch (selectedObjectType)
         {
-            case ObjectType.Primitive:
+            case TypeOfObject.Primitive:
 
                 selectedPrimitiveType = (PrimitiveType)EditorGUILayout.EnumPopup("Primitive Type", selectedPrimitiveType);
 
@@ -73,12 +101,14 @@ public class ObjectCreatorWindow : EditorWindow
 
                 if (GUILayout.Button("Place Primitive Manually"))
                 {
-
+                    previewObject = ObjectPlacer.CreatePreviewPrimitiveObject(selectedPrimitiveType);
+                    previewObject.layer = LayerMask.NameToLayer(PREVIEW_LAYER);
+                    isPlacingObject = true;
                 }
                 break;
 
-            case ObjectType.Prefab:
-                
+            case TypeOfObject.Prefab:
+
                 selectedPrefab = (GameObject)EditorGUILayout.ObjectField("Prefab", selectedPrefab, typeof(GameObject), false);
 
                 if (selectedPrefab != null && PrefabUtility.IsPartOfPrefabAsset(selectedPrefab))
@@ -94,6 +124,9 @@ public class ObjectCreatorWindow : EditorWindow
                     if (GUILayout.Button("Place Prefab Manually"))
                     {
 
+                        previewObject = ObjectPlacer.CreatePreviewPrefabObject(selectedPrefab);
+                        previewObject.layer = LayerMask.NameToLayer(PREVIEW_LAYER);
+                        isPlacingObject = true;
                     }
                 }
                 else
@@ -111,5 +144,85 @@ public class ObjectCreatorWindow : EditorWindow
         instance.transform.localPosition = objectPosition;
     }
 
-    
+    private void OnSceneGUI(SceneView sceneView)
+    {
+        currentEvent = Event.current;
+
+        if (isPlacingObject && previewObject != null)
+        {
+            Ray ray = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 400f, previewLayerMask))
+            {
+                if (hit.collider.gameObject != previewObject)
+                {
+                    previewObject.transform.position = hit.point;
+                }
+            }
+            else
+            {
+                Renderer previewRenderer = previewObject.GetComponent<Renderer>();
+                if (previewRenderer != null)
+                {
+                    objectHeight = previewRenderer.bounds.size.y * previewObject.transform.localScale.y;
+                }
+
+                //Debug.Log("Object height = " + objectHeight);
+
+                if (ray.direction.y != 0) // Check to avoid division by zero
+                {
+                    float distanceToGround = -ray.origin.y / ray.direction.y;
+                    Vector3 intersectionPoint = ray.origin + ray.direction * distanceToGround;
+
+                    // Set the preview object position to the intersection point
+                    previewObject.transform.position = new Vector3(intersectionPoint.x, objectHeight / 2, intersectionPoint.z);
+                }
+                else
+                {
+                    // Handle cases where ray direction is parallel to the ground (y == 0)
+                    previewObject.transform.position = new Vector3(ray.origin.x, objectHeight / 2, ray.origin.z); // or keep it at the original ray position
+                }
+            }
+
+            if (currentEvent.type == EventType.MouseDown && currentEvent.button == 0)
+            {
+                if(selectedObjectType == TypeOfObject.Primitive)
+                {
+                    ObjectPlacer.PlaceObject(previewObject, selectedPrimitiveType);
+                }
+                else if (selectedObjectType == TypeOfObject.Prefab)
+                {
+                    ObjectPlacer.PlaceObject(previewObject, selectedPrefab);
+                }
+
+                DestroyImmediate(previewObject);
+                isPlacingObject = false;
+                currentEvent.Use();
+            }
+
+            sceneView.Repaint();
+        }
+    }
+
+    private void CreatePreviewLayer()
+    {
+        SerializedObject tagManager = new SerializedObject(AssetDatabase.LoadAllAssetsAtPath("ProjectSettings/TagManager.asset")[0]);
+        SerializedProperty layersProp = tagManager.FindProperty("layers");
+
+        for (int i = 0; i < layersProp.arraySize; i++)
+        {
+            SerializedProperty layer = layersProp.GetArrayElementAtIndex(i);
+            if (string.IsNullOrEmpty(layer.stringValue))
+            {
+                layer.stringValue = PREVIEW_LAYER; // Assign the layer name
+                break;
+            }
+        }
+        tagManager.ApplyModifiedProperties();
+    }
+
+    private bool LayerExists(string layerName)
+    {
+        return LayerMask.NameToLayer(layerName) != -1;
+    }
 }
--- Assets/Editor/ObjectPlacer.cs
+++ Assets/Editor/ObjectPlacer.cs
@@ -2,20 +2,68 @@ using UnityEngine;
 using UnityEditor;
 
 
-public class ObjectPlacer : MonoBehaviour
+public static class ObjectPlacer
 {
-
-    // Store whether to show the Gizmo
-    public bool showGizmo = true;
-
-    private void Update()
+    public static GameObject CreatePreviewPrimitiveObject(PrimitiveType selectedPrimitiveType)
     {
-        // This logic might be different since we're in editor mode
-        if (showGizmo)
+        GameObject previewObject = GameObject.CreatePrimitive(selectedPrimitiveType);
+        SetObjectTransparency(previewObject, 0.5f);
+        return previewObject;
+    }
+
+    public static GameObject CreatePreviewPrefabObject(GameObject selectedPrefab)
+    {
+        GameObject previewObject = (GameObject)PrefabUtility.InstantiatePrefab(selectedPrefab);
+        SetObjectTransparency(previewObject, 0.5f);
+        return previewObject;
+    }
+
+    private static void SetObjectTransparency(GameObject previewObject, float transparency)
+    {
+        Renderer[] renderers = previewObject.GetComponentsInChildren<Renderer>();
+
+        foreach (Renderer renderer in renderers)
         {
-            Vector3 mousePosition = Event.current.mousePosition;
-            mousePosition.y = SceneView.lastActiveSceneView.position.height - mousePosition.y;
-            mousePosition.z = 0;
+            foreach (var material in renderer.sharedMaterials)
+            {
+                material.SetFloat("_Mode", 3); // Transparent mode for Standard Shader
+                material.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                material.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                material.SetInt("_ZWrite", 0);
+                material.DisableKeyword("_ALPHATEST_ON");
+                material.EnableKeyword("_ALPHABLEND_ON");
+                material.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                material.renderQueue = 3000;
+
+                Color color = material.color;
+                color.a = transparency;
+                material.color = color;
+            }
+        }
+    }
+
+    public static void PlaceObject(GameObject previewObject, PrimitiveType selectedPrimitiveType)
+    {
+        // Instantiate the final object at the preview object's position
+        GameObject finalObject = GameObject.CreatePrimitive(selectedPrimitiveType);
+
+        if (finalObject != null)
+        {
+            finalObject.transform.position = previewObject.transform.position;
+            SetObjectTransparency(finalObject, 1f);
+            Undo.RegisterCreatedObjectUndo(finalObject, "Place Object");
+        }
+    }
+
+    public static void PlaceObject(GameObject previewObject, GameObject selectedPrefab)
+    {
+        GameObject finalObject = (GameObject)PrefabUtility.InstantiatePrefab(selectedPrefab);
+
+        if (finalObject != null)
+        {
+            finalObject.transform.position = previewObject.transform.position;
+            SetObjectTransparency(finalObject, 1f);
+            Undo.RegisterCreatedObjectUndo(finalObject, "Place Object");
         }
     }
 }
