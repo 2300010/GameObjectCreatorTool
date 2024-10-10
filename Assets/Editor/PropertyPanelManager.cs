e2e28f2bbfbeb5500b8edaae421561cf6c7262ff
Sam <samabel18@gmail.com>
2024-10-10 17:04:15 -0400

    Start of the shift feature (UNFINISHED)
    TO DETAIL LATER

---
+++ Assets/Editor/PropertyPanelManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PropertyPanelManager
+{
+    private static PropertyPanelManager instance;
+
+    public static PropertyPanelManager Instance 
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = new PropertyPanelManager();
+            }
+            return instance;
+        } 
+    }
+
+    public string ObjectName { get => objectName; set => objectName = value; }
+    public float ObjectScale { get => objectScale; set => objectScale = value; }
+    public Vector3 ObjectPosition { get => objectPosition; set => objectPosition = value; }
+
+    private string objectName = "New Object";
+    private float objectScale = 1f;
+    private Vector3 objectPosition = Vector3.zero;
+
+    public void SetDefaultObjectParameters()
+    {
+        GUI.FocusControl(null);
+
+        ObjectName = "New Object";
+        ObjectScale = 1f;
+        ObjectPosition = Vector3.zero;
+    }
+
+    public void SetGameObjectParameters(GameObject instance, bool isPlacingObject)
+    {
+        if (!isPlacingObject)
+        {
+            instance.name = objectName;
+            instance.transform.localScale = Vector3.one * objectScale;
+            instance.transform.localPosition = objectPosition;
+        }
+        else
+        {
+            instance.name = objectName;
+            instance.transform.localScale = Vector3.one * objectScale;
+        }
+    }
+}
