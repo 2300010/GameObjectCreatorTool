98dc1345cbf0412c41cde719fa9aea3ac7eb00b8
2300010 <94806803+2300010@users.noreply.github.com>
2025-01-10 15:23:33 -0500

    MouseDelta screen to world FIXED (UNTESTED)
    EditorParametersManager class
    - Methods GetSceneViewSize & GetScreenToSceneMouseDelta ADDED (UNTESTED)
    - Class made Static

--- Assets/Editor/EditorParametersManager.cs
+++ Assets/Editor/EditorParametersManager.cs
@@ -3,10 +3,33 @@ using UnityEngine;
 
 public static class EditorParametersManager
 {
-    public static Vector2 GetCurrentSceneView()
+    public static Vector2 GetSceneViewSize()
     {
-        return Vector2.zero;
+        if (SceneView.lastActiveSceneView == null)
+        {
+            Debug.LogWarning("No active scene view found!");
+            return Vector2.zero;
+        }
+
+        Rect currentSceneView = SceneView.lastActiveSceneView.position;
+        return new Vector2(currentSceneView.width, currentSceneView.height);
     }
 
-    //public static float DistanceBetweenObjectAnd
+    public static Vector3 GetScreenToSceneMouseDelta(Vector2 screenMouseDelta, Vector3 currentObjectPosition)
+    {
+        if (!SceneView.lastActiveSceneView || !SceneView.lastActiveSceneView.camera)
+        {
+            Debug.LogWarning("No active scene view or camera was found!");
+            return Vector3.zero;
+        }
+
+        Camera editorCamera = SceneView.lastActiveSceneView.camera;
+
+        float distanceFromCameraToObject = Vector3.Distance(editorCamera.transform.position, currentObjectPosition);
+
+        Vector3 mouseStartingPoint = new Vector3(screenMouseDelta.x, screenMouseDelta.y, distanceFromCameraToObject);
+        Vector3 worldMouseDelta = editorCamera.ScreenToWorldPoint(mouseStartingPoint) - editorCamera.ScreenToWorldPoint(Vector3.zero);
+
+        return worldMouseDelta;
+    }
 }
