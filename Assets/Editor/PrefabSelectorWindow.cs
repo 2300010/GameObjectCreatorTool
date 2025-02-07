597c54be57cbeb03c6d85f899fbab359fab64fce
2300010 <94806803+2300010@users.noreply.github.com>
2025-02-07 15:55:26 -0500

    Second Window Test UNFINISHED
    PrefabSelectorWindow class CREATED

---
+++ Assets/Editor/PrefabSelectorWindow.cs
@@ -0,0 +1,14 @@
+using System;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEditor.PackageManager.UI;
+
+public class PrefabSelectorWindow : EditorWindow
+{
+
+
+    public static void ShowWindow()
+    {
+        //GetWindow<PrefabSelectorWindow>("Prefab Selector", typeof());
+    }
+}
